8016f5aaeac7463cb278d8003c4cd49043285538
Maedea9 <melaniecascante.j@gmail.com>
2023-03-06 16:32:53 -0500

    Modify mobile menu

--- mobile.js
+++ mobile.js
@@ -1,11 +1,21 @@
 let eventMobile = document.querySelector('.ic_menu')
 
 let target1 = document.querySelector('.fa-sharp')
-console.log(target1);
 let target2 = document.querySelector('.fa-xmark')
-console.log(target2);
+
+let modal = document.querySelector('.menu-mobile')
+
+let helloMobile = document.getElementById('#link1')*/
+/*let helloMobile = document.querySelector('.menu-mobile a')*/
+let portfolioMobile = document.querySelector()
 
 eventMobile.addEventListener('click', function(){ 
   target1.classList.toggle('hide')
   target2.classList.toggle('show')
+  modal.classList.toggle('hide')
+})
+
+helloMobile.addEventListener('click', function(e){
+  modal.classList.toggle('hide')
+  console.log(e.target);
 })
