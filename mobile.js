4d57e425ac7037268a1474fcf04b12e68f1395a3
Maedea9 <melaniecascante.j@gmail.com>
2023-03-11 03:38:49 -0500

    Fix linter errors

--- mobile.js
+++ mobile.js
@@ -1,51 +1,104 @@
-document.documentElement.addEventListener("click", (e) => {
-  const hamburguerIcon = document.querySelector(".fa-sharp");
-  const closeIcon = document.querySelector(".closed");
-  const menuIcon = document.querySelector(".menu-mobile");
+const projects = [
+  {
+    image: 'images/Rectangle21.png',
+    name: 'Project 1',
+    languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    linkdemo: 'https://maedea9.github.io/portfolio1/',
+    github: 'https://github.com/Maedea9/portfolio1',
+  },
+  {
+    image: 'images/Rectangle21v.png',
+    name: 'Project 2',
+    languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    linkdemo: 'https://maedea9.github.io/portfolio1/',
+    github: 'https://github.com/Maedea9/portfolio1',
+  },
+  {
+    image: 'images/Rectangle21v2.png',
+    name: 'Project 3',
+    languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    linkdemo: 'https://maedea9.github.io/portfolio1/',
+    github: 'https://github.com/Maedea9/portfolio1',
+  },
+  {
+    image: 'images/Rectangle21(1).png',
+    name: 'Project 4',
+    languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    linkdemo: 'https://maedea9.github.io/portfolio1/',
+    github: 'https://github.com/Maedea9/portfolio1',
+  },
+  {
+    image: 'images/Rectangle21(2).png',
+    name: 'Project 5',
+    languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    linkdemo: 'https://maedea9.github.io/portfolio1/',
+    github: 'https://github.com/Maedea9/portfolio1',
+  },
+  {
+    image: 'images/Rectangle21(3).png',
+    name: 'Project 6',
+    languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    linkdemo: 'https://maedea9.github.io/portfolio1/',
+    github: 'https://github.com/Maedea9/portfolio1',
+  },
+];
 
-  console.log(e.target)
-  if (e.target.matches("#link1")) {
-    menuIcon.classList.toggle("hide");
-    hamburguerIcon.classList.remove("hide");
-    closeIcon.classList.remove("show");
-  }
-  if (e.target.matches("#link2")) {
-    menuIcon.classList.toggle("hide");
-    hamburguerIcon.classList.remove("hide");
-    closeIcon.classList.remove("show");
-  }
-  if (e.target.matches("#link3")) {
-    menuIcon.classList.toggle("hide");
-    hamburguerIcon.classList.remove("hide");
-    closeIcon.classList.remove("show");
-  }
-  if (e.target.matches("#link4")) {
-    menuIcon.classList.toggle("hide");
-    hamburguerIcon.classList.remove("hide");
-    closeIcon.classList.remove("show");
-  }
-  if (e.target.matches(".see-project")) {
-    const modalWindow = document.querySelector(".modal-window");
-    modalWindow.style.display = "block";
-  }
-  if (e.target.matches(".close-xbtn")) {
-    const modalWindow = document.querySelector(".modal-window");
-    modalWindow.style.display = "none";
-  }
-  if(e.target.matches('.ic_menu *')){    
-    hamburguerIcon.classList.toggle("hide");
-    closeIcon.classList.toggle("show");
-    menuIcon.classList.toggle("hide");
-  }
+document.documentElement.addEventListener('click', (e) => {
+  const hamburguerIcon = document.querySelector('.fa-sharp');
+  const closeIcon = document.querySelector('.closed');
+  const menuIcon = document.querySelector('.menu-mobile');
 
+  if (e.target.matches('#link1')) {
+    menuIcon.classList.toggle('hide');
+    hamburguerIcon.classList.remove('hide');
+    closeIcon.classList.remove('show');
+  }
+  if (e.target.matches('#link2')) {
+    menuIcon.classList.toggle('hide');
+    hamburguerIcon.classList.remove('hide');
+    closeIcon.classList.remove('show');
+  }
+  if (e.target.matches('#link3')) {
+    menuIcon.classList.toggle('hide');
+    hamburguerIcon.classList.remove('hide');
+    closeIcon.classList.remove('show');
+  }
+  if (e.target.matches('#link4')) {
+    menuIcon.classList.toggle('hide');
+    hamburguerIcon.classList.remove('hide');
+    closeIcon.classList.remove('show');
+  }
+  if (e.target.matches('.see-project')) {
+    const modalWindow = document.querySelector('.modal-window');
+    modalWindow.style.display = 'block';
+  }
+  if (e.target.matches('.close-xbtn')) {
+    const modalWindow = document.querySelector('.modal-window');
+    modalWindow.style.display = 'none';
+  }
+  if (e.target.matches('.ic_menu *')) {
+    hamburguerIcon.classList.toggle('hide');
+    closeIcon.classList.toggle('show');
+    menuIcon.classList.toggle('hide');
+  }
 });
 
 /// // SEE-THIS-PROJECT///
 
-// const seeThisProject = document.querySelector(".see-project");
-
 function showModal() {
-  const modalWindow = document.querySelector(".body");
+  const modalWindow = document.querySelector('.body');
   const worksHtml = `<div class="modal-window px-2">
 <div class="pop-work-card">
 <div class="pop-header p-0 d-flex flex-column flex-md-row-reverse w-100">
@@ -80,87 +133,15 @@ function showModal() {
   modalWindow.innerHTML += worksHtml;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  showModal();  
+document.addEventListener('DOMContentLoaded', () => {
+  showModal();
 });
-
-// function createprojects() {
-//   for (let i = 0; i < projects.length; i += 1) {}
-//   const openModalBtn = document.querySelectorAll(".see-project");
-//   openModalBtn.forEach((element) => {
-//     element.addEventListener("click", () => {});
-//   });
-// }
 
 // -------------------------------------------------create projects
 
-// general variables
+const container = document.querySelector('.projects');
 
-// card info
-
-const projects = [
-  {
-    image: "images/Rectangle21.png",
-    name: "Project 1",
-    languages: ["Html/Css", "Ruby on Rails", "JavaScript"],
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-    linkdemo: "https://maedea9.github.io/portfolio1/",
-    github: "https://github.com/Maedea9/portfolio1",
-  },
-  {
-    image: "images/Rectangle21v.png",
-    name: "Project 2",
-    languages: ["Html/Css", "Ruby on Rails", "JavaScript"],
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-    linkdemo: "https://maedea9.github.io/portfolio1/",
-    github: "https://github.com/Maedea9/portfolio1",
-  },
-  {
-    image: "images/Rectangle21v2.png",
-    name: "Project 3",
-    languages: ["Html/Css", "Ruby on Rails", "JavaScript"],
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-    linkdemo: "https://maedea9.github.io/portfolio1/",
-    github: "https://github.com/Maedea9/portfolio1",
-  },
-  {
-    image: "images/Rectangle21(1).png",
-    name: "Project 4",
-    languages: ["Html/Css", "Ruby on Rails", "JavaScript"],
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-    linkdemo: "https://maedea9.github.io/portfolio1/",
-    github: "https://github.com/Maedea9/portfolio1",
-  },
-  {
-    image: "images/Rectangle21(2).png",
-    name: "Project 5",
-    languages: ["Html/Css", "Ruby on Rails", "JavaScript"],
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-    linkdemo: "https://maedea9.github.io/portfolio1/",
-    github: "https://github.com/Maedea9/portfolio1",
-  },
-  {
-    image: "images/Rectangle21(3).png",
-    name: "Project 6",
-    languages: ["Html/Css", "Ruby on Rails", "JavaScript"],
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-    linkdemo: "https://maedea9.github.io/portfolio1/",
-    github: "https://github.com/Maedea9/portfolio1",
-  },
-];
-
-// card template
-
-// container
-let container = document.querySelector(".projects");
-
-let gridArea1 = `
+const gridArea1 = `
   <div class="grid-area1">
   <h2 id="project-title">Projects</h2>
   </div>
@@ -168,8 +149,8 @@ let gridArea1 = `
 container.innerHTML = gridArea1;
 
 function createprojects() {
-  for (let i = 0; i < projects.length; i++) {
-    let cardTemplate = `
+  for (let i = 0; i < projects.length; i += 1) {
+    const cardTemplate = `
 <img class="image1" src="${projects[i].image}" alt="project-1" width="100%">
 <div class="work-description ">
   <h3> ${projects[i].name} </h3>
@@ -184,29 +165,29 @@ function createprojects() {
 </div>
 `;
 
-    let article = document.createElement("article");
+    const article = document.createElement('article');
     article.className = `works w${i}`;
     article.innerHTML = cardTemplate;
     container.appendChild(article);
 
-    if (i == 1) {
-      let desktopimg = document.createElement("div");
-      desktopimg.className = `desktop-geoma`;
+    if (i === 1) {
+      const desktopimg = document.createElement('div');
+      desktopimg.className = 'desktop-geoma';
       container.appendChild(desktopimg);
     }
 
-    if (i == 2) {
-      let desktopimg1 = document.createElement("div");
-      desktopimg1.className = `desktop-geomc`;
+    if (i === 2) {
+      const desktopimg1 = document.createElement('div');
+      desktopimg1.className = 'desktop-geomc';
       container.appendChild(desktopimg1);
-      let desktopimg2 = document.createElement("div");
-      desktopimg2.className = `desktop-geomd`;      
+      const desktopimg2 = document.createElement('div');
+      desktopimg2.className = 'desktop-geomd';
       container.appendChild(desktopimg2);
     }
 
-    if (i == 4) {
-      let desktopimg = document.createElement("div");
-      desktopimg.className = `desktop-geomb`;
+    if (i === 4) {
+      const desktopimg = document.createElement('div');
+      desktopimg.className = 'desktop-geomb';
       container.appendChild(desktopimg);
     }
   }
