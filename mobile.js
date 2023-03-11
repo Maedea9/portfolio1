6f29267795a084c567e98d58aac781eca515d4f3
benja27 <50673336+benja27@users.noreply.github.com>
2023-03-11 17:19:29 -0600

    Add modal window

--- mobile.js
+++ mobile.js
@@ -1,19 +1,16 @@
-const projects = [
-  {
+const projects = [{
     image: 'images/Rectangle21.png',
-    name: 'Project name',
+    name: 'Project name 1',
     languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     linkdemo: 'https://maedea9.github.io/portfolio1/',
     github: 'https://github.com/Maedea9/portfolio1',
   },
   {
     image: 'images/Rectangle21v.png',
-    name: 'Project name',
+    name: 'Project name 2',
     languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     linkdemo: 'https://maedea9.github.io/portfolio1/',
     github: 'https://github.com/Maedea9/portfolio1',
   },
@@ -21,8 +18,7 @@ const projects = [
     image: 'images/Rectangle21v2.png',
     name: 'Project name',
     languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     linkdemo: 'https://maedea9.github.io/portfolio1/',
     github: 'https://github.com/Maedea9/portfolio1',
   },
@@ -30,8 +26,7 @@ const projects = [
     image: 'images/Rectangle21(1).png',
     name: 'Project name',
     languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     linkdemo: 'https://maedea9.github.io/portfolio1/',
     github: 'https://github.com/Maedea9/portfolio1',
   },
@@ -39,8 +34,7 @@ const projects = [
     image: 'images/Rectangle21(2).png',
     name: 'Project name',
     languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     linkdemo: 'https://maedea9.github.io/portfolio1/',
     github: 'https://github.com/Maedea9/portfolio1',
   },
@@ -48,14 +42,13 @@ const projects = [
     image: 'images/Rectangle21(3).png',
     name: 'Project name',
     languages: ['Html/Css', 'Ruby on Rails', 'JavaScript'],
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     linkdemo: 'https://maedea9.github.io/portfolio1/',
     github: 'https://github.com/Maedea9/portfolio1',
   },
 ];
 
- document.documentElement.addEventListener('click', (e) => {
+document.documentElement.addEventListener('click', (e) => {
   const hamburguerIcon = document.querySelector('.fa-sharp');
   const closeIcon = document.querySelector('.closed');
   const menuIcon = document.querySelector('.menu-mobile');
@@ -80,12 +73,52 @@ const projects = [
     hamburguerIcon.classList.remove('hide');
     closeIcon.classList.remove('show');
   }
-  if (e.target.matches('.see-project')) {
-    const modalWindow = document.querySelector('.modal-window');
+  if (e.target.matches('.see-project')) {        
+    let nboton = e.srcElement.getAttribute('data-id')
+    createmodal(nboton)
+    
+    function createmodal(n) {
+      let i = n || 0
+    
+      const container = document.querySelector('.modal-container');
+    
+      let worksHtml = ``;
+      
+      worksHtml += `<div class="modal-window px-2">
+          <div class="pop-work-card">
+            <div class="pop-header p-0 d-flex flex-column flex-md-row-reverse w-100">
+              <button class="close-xbtn fa-solid fa-xmark align-self-end m-0"></button>
+              <div class="title m-0 p-0">${projects[i].name}</div>
+            </div>
+            <div class="pop-tech-container m-0 mt-3">
+              <ul class="ps-0 d-flex gap-1" >
+                <li class="pop-language1 sm w-100">${projects[i].languages[0]}</li>
+                <li class="pop-language2 sm w-100">${projects[i].languages[1]}</li>
+                <li class="pop-language3 sm w-100">${projects[i].languages[2]}</li>
+              </ul>
+            </div>
+            <img class="pop-img1 p-0 w-100" src=${projects[i].image} alt="">
+            <div class="mx-0 modal-text-cont mt-4" >
+              <p class="pop-text mx-auto p-0">${projects[i].description}</p>
+              <div class="live-source-btns mx-auto"> 
+                <a href=${projects[i].linkdemo} class="see-live">See Live</a>
+                <a href=${projects[i].github} class="see-source">See Source</a>
+              </div> 
+            </div>
+            <div class="pop-desktop-btns">
+              <button class="previous-work"><i class="fa-solid fa-arrow-left"></i>Previous project</button>
+              <button class="next-work">Next project<i class="fa-solid fa-arrow-right"></i></button>
+            </div>
+          </div>
+      </div>`;
+      container.innerHTML = worksHtml;
+    }
+    
+    const modalWindow = document.querySelector('.modal-container');
     modalWindow.style.display = 'block';
   }
   if (e.target.matches('.close-xbtn')) {
-    const modalWindow = document.querySelector('.modal-window');
+    const modalWindow = document.querySelector('.modal-container');
     modalWindow.style.display = 'none';
   }
   if (e.target.matches('.ic_menu *')) {
@@ -97,11 +130,14 @@ const projects = [
 
 /// // SEE-THIS-PROJECT///
 
-function showModal() {
-  const modalWindow = document.querySelector('.body');
-  let worksHtml = '';
-  for (let i = 0; i < projects.length; i++) {
-    worksHtml += `<div class="modal-window px-2">
+function createmodal(n) {
+  let i = n || 0
+
+  const container = document.querySelector('.modal-container');
+
+  let worksHtml = ``;
+  
+  worksHtml += `<div class="modal-window px-2">
       <div class="pop-work-card">
         <div class="pop-header p-0 d-flex flex-column flex-md-row-reverse w-100">
           <button class="close-xbtn fa-solid fa-xmark align-self-end m-0"></button>
@@ -127,13 +163,13 @@ function showModal() {
           <button class="next-work">Next project<i class="fa-solid fa-arrow-right"></i></button>
         </div>
       </div>
-    </div>`;
-  }
-  modalWindow.innerHTML += worksHtml;
+  </div>`;
+  container.innerHTML = worksHtml;
 }
 
+
 document.addEventListener('DOMContentLoaded', () => {
-  showModal();
+  createmodal();
 });
 
 // -------------------------------------------------create projects
@@ -159,7 +195,7 @@ function createprojects() {
     <li class="item3"> ${projects[i].languages[2]} </li>
   </ul>
   <div class="item4">
-    <button class="see-project">See this project <i class="fa-solid fa-arrow-right"></i></button>
+    <button data-id=${i} class="see-project">See this project <i class="fa-solid fa-arrow-right"></i></button>
   </div>
 </div>
 `;
@@ -194,11 +230,3 @@ function createprojects() {
 
 createprojects();
 
-const seeProject = document.querySelectorAll('.see-project');
-
-for (let i = 0; i < projects.length; i += 1) {
-  seeProject[i].addEventListener('click', () => {
-    alert(123);
-    //const modalWindow = document.querySelector('.modal-window');
-  });
-}
